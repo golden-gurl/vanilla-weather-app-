d2684d11a015bdf7d26951424762c8003262cb54
golden-gurl <106713245+golden-gurl@users.noreply.github.com>
2022-07-19 17:35:09 -0400

    working temp change only

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
+function displayTemp(response) {
+  console.log(response.data.main.temp);
+  let tempElement = document.querySelector("#Temp");
+  tempElement.innerHTML = response.data.main.temp;
+}
+
 let apiKey = `5d5058b9c4b2f291c2f88009101bb780`;
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
 console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemp);
