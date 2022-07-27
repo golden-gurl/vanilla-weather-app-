3b14c1337738065fdcfafbf80a0f14ca10ac70b6
golden-gurl <106713245+golden-gurl@users.noreply.github.com>
2022-07-26 21:00:28 -0400

    weather icon now pulls live data

--- src/index.js
+++ src/index.js
@@ -1,16 +1,49 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours} : ${minutes}`;
+}
+
 function displayTemp(response) {
-  console.log(response.data);
   let cityElement = document.querySelector("#cityName");
   let tempElement = document.querySelector("#Temp");
   let condtionElement = document.querySelector("#cityCondition");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#weather-icon");
 
   tempElement.innerHTML = response.data.main.temp;
   cityElement.innerHTML = response.data.name;
   condtionElement.innerHTML = response.data.weather[0].description;
-  humidityElement.innerHTML = response.data.main.humidity;
-  windElement.innerHTML = response.data.wind.speed;
+  humidityElement.innerHTML = Math.round(response.data.main.humidity);
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute(
+    "alt",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].description}@2x.png`
+  );
 }
 
 let apiKey = `5d5058b9c4b2f291c2f88009101bb780`;
